0ea974ad37f4a7fa9dd19b26b83c00d35fa017af
William Takayama <william-sobreira@hotmail.com>
2021-04-22 23:11:47 -0300

    (Feat) using axios and normalizing episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,65 @@
-import { InferGetStaticPropsType } from "next";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { GetStaticProps } from "next";
 import React from "react";
+import HomePageComponent from "../components/PageComponents/Home/HomePageComponent";
+import { Episode } from "../interfaces/Episodes";
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home({
-  episodes,
-}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
-  return (
-    <div>
-      <h1>index</h1>
-      <pre>{JSON.stringify(episodes, null, 2)}</pre>
-    </div>
-  );
+interface HomeProps {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export default function Home({ ...rest }: HomeProps): JSX.Element {
+  return <HomePageComponent {...rest} />;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get<Episode[]>("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data?.map((episode) => {
+    const {
+      id,
+      title,
+      members,
+      published_at,
+      thumbnail,
+      description,
+      file,
+    } = episode;
+
+    return {
+      id,
+      title,
+      members,
+      publishedAt: format(parseISO(published_at), "d MMM yy", { locale: ptBR }),
+      thumbnail,
+      description,
+      file: {
+        url: file.url,
+        type: file.type,
+        duration: Number(file.duration),
+        durationAsString: convertDurationToTimeString(Number(file.duration)),
+      },
+    };
+  });
+
+  const latestEpisodes = episodes?.slice(0, 2);
+  const allEpisodes = episodes?.slice(2, episodes?.length);
 
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
